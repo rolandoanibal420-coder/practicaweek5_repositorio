dbb1e107e08896f8fe10073a2011860c64f348d4
ROLANDO ANIBAL AGUILAR NAVARRO <raguilarn@ucenfotec.ac.cr>
2026-02-24 16:30:59 -0600

    :new: constante y comentarios en Java
    aprendimos a hacer comentarios y declarar constantes en javascript usando visual studio code

---
+++ formularios/js/IMC.js
@@ -0,0 +1,16 @@
+/*
+comentario multilinea
+*/
+
+
+console.log("mensaje en la consola");
+console.log("otro mensaje");
+
+// constante: valor que NO cambia durante la ejecucion del codigo
+const PI = 3.14;
+console.log(PI);
+
+const TIPO_USUARIO = "Visitante;"
+console.log(TIPO_USUARIO);
+
+// variable: valor que cambia durante la ejecucion del codigo
